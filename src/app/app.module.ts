3ffc305cbb3b37a93ad43614da4eae2463c2502c
Koushik <koushk01@corp.pvt>
2018-02-01 17:51:30 +0530

    inital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatToolbarModule } from '@angular/material';
+
+import { JsonSchemaFormModule, MaterialDesignFrameworkModule, Bootstrap4FrameworkModule, Bootstrap3FrameworkModule, 
+  NoFrameworkModule     } from 'angular2-json-schema-form';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [ AppComponent ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, MatCardModule, MatToolbarModule,
+    MaterialDesignFrameworkModule , JsonSchemaFormModule.forRoot(MaterialDesignFrameworkModule)
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
